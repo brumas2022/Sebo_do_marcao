c5cf52df873fa5fd79e53d10a6400e899d39caf9
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2023-10-20 15:53:33 -0400

    Update index.py

--- index.py
+++ index.py
@@ -2,6 +2,7 @@ import streamlit as st
 st.header("Esta é a pagina inicial do Sebo do Marcão")
 st.write("---")
 st.markdown("vamos comecar a trabalhar nisso aqui")
+st.image("IMG_20220616_220024.jpg")
 a = st.selectbox("Escolha a opção desejada",("Mandar uma mensagem para o Marcao", "Desejar bom dia ao Marcao", "Desejar boa noite ao Marcao"))
 if a == "Mandar uma mensagem para o Marcao":
   nome = st.text_input("Digite seu nome")
