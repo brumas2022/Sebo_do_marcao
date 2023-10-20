1d0424181daaf05c5051b6babf8957738f301ebc
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2023-10-20 15:45:09 -0400

    Update index.py

--- index.py
+++ index.py
@@ -3,3 +3,11 @@ st.header("Esta é a pagina inicial do Sebo do Marcão")
 st.write("---")
 st.markdown("vamos comecar a trabalhar nisso aqui")
 a = st.selectbox("Escolha a opção desejada",("Mandar uma mensagem para o Marcao", "Desejar bom dia ao Marcao", "Desejar boa noite ao Marcao"))
+if a == "Mandar uma mensagem para o Marcao":
+  nome = st.text_input("Digite seu nome")
+  mensagem = st.text_imput("Digite a sua mensagem")
+elif a == "Desejar bom dia ao Marcao":
+  st.markdown("BOM DIA, MARCAO")
+elif a ==  "Desejar boa noite ao Marcao":
+  st.markdown("BOA NOITE, MARCAO")
+  
