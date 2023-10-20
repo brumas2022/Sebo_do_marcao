053328916f23184fcb6d9df0b3d439724b9da6dc
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2023-10-20 11:50:06 -0400

    Update index.py

--- index.py
+++ index.py
@@ -2,4 +2,4 @@ import streamlit as st
 st.header("Esta é a pagina inicial do Sebo do Marcão")
 st.write("---")
 st.markdown("vamos comecar a trabalhar nisso aqui")
-a = st.selectbox("Escolha a opção desejada",("Mandar uma mensagem para o Marcao", "Desejar bom dia ao Marcao", "Desejar boa noite ao Marcao")
+a = st.selectbox("Escolha a opção desejada",("Mandar uma mensagem para o Marcao", "Desejar bom dia ao Marcao", "Desejar boa noite ao Marcao"))
