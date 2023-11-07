d596dd28ad7c990beeaac4a63d9580e204269128
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2023-11-07 00:11:03 -0300

    Update index.py

--- index.py
+++ index.py
@@ -4,6 +4,8 @@ st.write("---")
 st.markdown("Aqui será possivel encontrar coisas com nexo e também totalmente sem nexo")
 st.image("IMG_20220616_220024.jpg")
 st.link_button("Loja paque menos", "https://mais.app/1Nm5y9")
+st.link_button("Memorando SANEAR", "https://www.sanearmt.com.br")
+st.link_button("Cadastro de animais", "https://causanimal.streamlit.app")
 a = st.selectbox("Escolha a opção desejada",("Mandar uma mensagem para o Marcao", "Desejar bom dia ao Marcao", "Desejar boa noite ao Marcao"))
 if a == "Mandar uma mensagem para o Marcao":
   nome = st.text_input("Digite seu nome")
@@ -12,7 +14,9 @@ if a == "Mandar uma mensagem para o Marcao":
       st.write(f"{nome} enviou a seguinte mensagem : {prompt}")
 elif a == "Desejar bom dia ao Marcao":
   st.markdown("BOM DIA, MARCAO")
+  st.markdown("Com fé, chegamos la")
 elif a ==  "Desejar boa noite ao Marcao":
   st.markdown("BOA NOITE, MARCAO")
-  st.markdown("Durma bem")
+  st.markdown("Durma com os anjos")
+  st.write("---")
   
