76db68709d321e26c78ac273d4c130abf999856b
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2023-11-09 15:24:48 -0400

    Update index.py

--- index.py
+++ index.py
@@ -5,10 +5,12 @@ st.markdown("Aqui será possivel encontrar coisas com nexo e também totalmente 
 st.write("---")
 st.write("Pensamento do dia: AINDA QUE EU FALASSE A LINGUA DOS HOMENS, SEM AMOR EU NADA SERIA")
 st.image("IMG_20220616_220024.jpg")
-st.link_button("Loja paque menos", "https://mais.app/1Nm5y9", use_container_width=True)
-st.link_button("Memorando SANEAR", "https://comunicasanear-avcnrpqesbpzeg2ecsmuun.streamlit.app/")
-st.link_button("Cadastro de animais", "https://causanimal.streamlit.app")
-st.link_button("Video do Youtube", "https://www.youtube.com/watch?v=JnxCIRxt3kQ&t=19s")
+
+cols = st.columns((1,1))
+cols[0].link_button("Loja paque menos", "https://mais.app/1Nm5y9", use_container_width=True)
+cols[0].link_button("Memorando SANEAR", "https://comunicasanear-avcnrpqesbpzeg2ecsmuun.streamlit.app/", use_container_width=True)
+cols[0].link_button("Cadastro de animais", "https://causanimal.streamlit.app", use_container_width=True)
+cols[1].link_button("Video do Youtube", "https://www.youtube.com/watch?v=JnxCIRxt3kQ&t=19s", use_container_width=True)
 a = st.selectbox("Escolha a opção desejada",("Mandar uma mensagem para o Marcao", "Desejar bom dia ao Marcao", "Desejar boa noite ao Marcao"))
 if a == "Mandar uma mensagem para o Marcao":
   nome = st.text_input("Digite seu nome")
