7c7372042340957f564377b6c2bd370fe4977b09
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2023-11-09 15:22:33 -0400

    Update index.py

--- index.py
+++ index.py
@@ -5,7 +5,7 @@ st.markdown("Aqui será possivel encontrar coisas com nexo e também totalmente 
 st.write("---")
 st.write("Pensamento do dia: AINDA QUE EU FALASSE A LINGUA DOS HOMENS, SEM AMOR EU NADA SERIA")
 st.image("IMG_20220616_220024.jpg")
-st.link_button("Loja paque menos", "https://mais.app/1Nm5y9")
+st.link_button("Loja paque menos", "https://mais.app/1Nm5y9", use_container_width=True)
 st.link_button("Memorando SANEAR", "https://comunicasanear-avcnrpqesbpzeg2ecsmuun.streamlit.app/")
 st.link_button("Cadastro de animais", "https://causanimal.streamlit.app")
 st.link_button("Video do Youtube", "https://www.youtube.com/watch?v=JnxCIRxt3kQ&t=19s")
