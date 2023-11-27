904388ff0e093c63275680027f5400556d685dc4
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2023-11-27 09:01:00 -0400

    Update index.py

--- index.py
+++ index.py
@@ -8,6 +8,29 @@ st.write("---")
 st.write("---")
 st.image("IMG_20220616_220024.jpg")
 
+cols1 = st.columns((1,1,1))
+cols1[0].image("livros-stack-realistic_1284-4735.jpg")
+cols1[1].image("livros-stack-realistic_1284-4735.jpg")
+cols1[2].image("livros-stack-realistic_1284-4735.jpg")
+
+cols2 = st.columns((1,1,1,1,1,1,1,1,1,1))
+cols2[0].image("simbolos-musicais-ondulam-em-fundo-branco_1308-77408")
+cols2[1].image("simbolos-musicais-ondulam-em-fundo-branco_1308-77408")
+cols2[2].image("simbolos-musicais-ondulam-em-fundo-branco_1308-77408")
+cols2[3].image("simbolos-musicais-ondulam-em-fundo-branco_1308-77408")
+cols2[4].image("simbolos-musicais-ondulam-em-fundo-branco_1308-77408")
+cols2[5].image("simbolos-musicais-ondulam-em-fundo-branco_1308-77408")
+cols2[6].image("simbolos-musicais-ondulam-em-fundo-branco_1308-77408")
+cols2[7].image("simbolos-musicais-ondulam-em-fundo-branco_1308-77408")
+cols2[8].image("simbolos-musicais-ondulam-em-fundo-branco_1308-77408")
+cols2[9].image("simbolos-musicais-ondulam-em-fundo-branco_1308-77408")
+
+                   
+                   
+
+
+
+
 cols = st.columns((1,1))
 cols[0].link_button("Loja paque menos", "https://mais.app/1Nm5y9", use_container_width=True)
 cols[0].link_button("Memorando SANEAR", "https://comunicasanear-avcnrpqesbpzeg2ecsmuun.streamlit.app/", use_container_width=True)
@@ -15,10 +38,7 @@ cols[0].link_button("Cadastro de animais", "https://causanimal.streamlit.app", u
 cols[1].link_button("Video do Youtube", "https://www.youtube.com/watch?v=JnxCIRxt3kQ&t=19s", use_container_width=True)
 cols[1].link_button("Faça sua doação", "https://www.mercadopago.com.br/integrations/v1/web-payment-checkout.js", use_container_width=True)
 
-cols1 = st.columns((1,1,1))
-cols1[0].image("livros-stack-realistic_1284-4735.jpg")
-cols1[1].image("livros-stack-realistic_1284-4735.jpg")
-cols1[2].image("livros-stack-realistic_1284-4735.jpg")
+
 
 a = st.selectbox("Escolha a opção desejada",("Mensagem para o Marcao", "Desejar bom dia ao Marcao", "Desejar boa noite ao Marcao"))
 if a == "Mensagem para o Marcao":
