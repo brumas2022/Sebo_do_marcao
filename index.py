12f0f25554ec36b9fc546bc8c904320ed7c51e0c
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2023-12-04 15:34:25 -0400

    Update index.py

--- index.py
+++ index.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from st_pages import show_pages_from_config, add_page_title
 
-add_page_title()
-show_pages_from_config(".streamlit/pages.toml")
+#add_page_title()
+#show_pages_from_config(".streamlit/pages.toml")
 
 
 st.header(":green[Sebo do Marcão]", divider="orange")
