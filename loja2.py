39cb996b9f0e7386db65e33775cc0267c3b3f35b
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2023-12-04 10:54:54 -0400

    Update loja2.py

--- loja2.py
+++ loja2.py
@@ -2,5 +2,5 @@ import streamlit as st
 nome = st.text_input("entre com login")
 senha = st.text_input("entre com a senha")
 if senha=="1234":
-   st.write(nome+"  clique no botao para acessar o site')
+   st.write(nome+"  clique no botao para acessar o site")
    st.link_button("Memorando agora", "https://comunicasanear-avcnrpqesbpzeg2ecsmuun.streamlit.app/")
