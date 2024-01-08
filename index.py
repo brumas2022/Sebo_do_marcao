82e78cd11e50c87f69a60e6815adcb47274cc6d1
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-01-08 14:48:15 -0400

    Update index.py

--- index.py
+++ index.py
@@ -50,11 +50,12 @@ cols2[6].image("simbolos-musicais-ondulam-em-fundo-branco_1308-77408.jpg")
 
 
 cols = st.columns((1,1))
-cols[0].link_button("Loja pague menos", "https://mais.app/1Nm5y9", use_container_width=True)
+cols[0].link_button("Pague menos", "https://mais.app/1Nm5y9", use_container_width=True)
 cols[0].link_button("Memorando SANEAR", "https://comunicasanear-avcnrpqesbpzeg2ecsmuun.streamlit.app/", use_container_width=True)
 cols[0].link_button("Cadastro de animais", "https://causanimal.streamlit.app", use_container_width=True)
 cols[1].link_button("Video do Youtube", "https://www.youtube.com/watch?v=JnxCIRxt3kQ&t=19s", use_container_width=True)
 cols[1].link_button("Faça sua doação", "https://www.mercadopago.com.br/integrations/v1/web-payment-checkout.js", use_container_width=True)
+cols[1].link_button("Lojinha", "https://loja1dosebo.streamlit.app/", use_container_width=True)
 
 
 
