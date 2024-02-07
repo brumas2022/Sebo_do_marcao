ed45655f6ad831ceeeb79539c73b91731d9cecc9
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-02-07 16:49:46 -0400

    Update loja1.py

--- loja1.py
+++ loja1.py
@@ -3,6 +3,9 @@ import telebot
 st.markdown("RECANTO DOS USADOS")
 st.image("IMG_20220616_220024.jpg")
 st.markdown("PREÇO : 250,00")
+CHAVE_API = "6253633084:AAEDJfuYUyNWydjAJR4dsZUh3vZGmLfSUps"
+
+bot = telebot.TeleBot(CHAVE_API)
 
 
 
