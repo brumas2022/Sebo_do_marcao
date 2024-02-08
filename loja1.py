d7028585ba2bc1193d3367bc8a6ef6619af0ed68
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-02-08 10:21:25 -0400

    Update loja1.py

--- loja1.py
+++ loja1.py
@@ -7,6 +7,9 @@ CHAVE_API = "6253633084:AAEDJfuYUyNWydjAJR4dsZUh3vZGmLfSUps"
 
 bot = telebot.TeleBot(CHAVE_API)
 
+nome + st.text_input("Escreva sua mensagem")
+@bot.send_message(820304760, nome)
+
 @bot.message_handler(commands=["servico"])
 def servico(mensagem):
     bot.reply_to(mensagem, "Este servico ficara disponivel a partir de amanha")
