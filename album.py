bd5555938b401780b19c9bfca12be4aa42e46b1f
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-02-27 07:04:44 -0400

    Atualizar o album.py

--- album.py
+++ album.py
@@ -11,4 +11,4 @@ CHAVE_API = "6369586964:AAFm3QLWBNnLYUwtl2xyqi0145oYJd8OHNc"
 bot = telebot.TeleBot(CHAVE_API)  
 
 if escolha=="ouvir":
-   bot.send_message(820304760, escolha)
+   bot.send_message(820304760, "tente ouvir mais do que falar")
