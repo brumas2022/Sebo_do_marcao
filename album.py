cd68ef0dab3d361166fc1755028ea4fa9cd288d8
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-02-26 21:19:34 -0400

    Atualizar o album.py

--- album.py
+++ album.py
@@ -6,6 +6,8 @@ st.markdown("Album dos caes abrigado na arpaa")
 st.markdown("aqui teremos fotos")
 
 
+CHAVE_API = "6369586964:AAFm3QLWBNnLYUwtl2xyqi0145oYJd8OHNc"
+
 bot = telebot.TeleBot(CHAVE_API)  
 
         bot.send_message(820304760, nome)
