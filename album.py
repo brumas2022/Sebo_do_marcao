0473052cbf7e80854176e01dcbfbed9ef0f18044
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-02-29 11:04:50 -0400

    Update album.py

--- album.py
+++ album.py
@@ -24,7 +24,7 @@ if escolha=="sair":
 
 @bot.message_handler(commands=["caes"])
 def caes(mensagem):
-   bot.send_message(820304760, "Voce entrou na area de caes. Cuidado!!)
+   bot.send_message(820304760, "Voce entrou na area de caes. Cuidado!!")
 
 
 
