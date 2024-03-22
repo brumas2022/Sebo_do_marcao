cb15c7d14a01c9b5e4ecb5bd027a70095595b01e
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-03-22 16:46:45 -0400

    Update abrigados.py

--- abrigados.py
+++ abrigados.py
@@ -15,7 +15,7 @@ st.dataframe(df)
 st.dataframe(df['foto'])
 
 col1, col2, col3, col4 = st.columns((1,1,1,1))
-col1.image(df['foto'][5], use_column_width="always")
+col1.image(df['foto'][5], caption=df['nome'][5], use_column_width="always")
 col2.image(df['foto'][2], use_column_width="always")
 col3.image(df['foto'][11], use_column_width="always")
 col4.image(df['foto'][12], use_column_width="always")
