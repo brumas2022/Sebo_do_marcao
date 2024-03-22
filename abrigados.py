d01a9960122149195622195a25c045f4407f9958
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-03-22 16:21:21 -0400

    Update abrigados.py

--- abrigados.py
+++ abrigados.py
@@ -7,9 +7,9 @@ st.set_page_config(layout="wide")
 
 st.header("Animais abrigados na ARPAA")
 
-engine = create_engine('postgresql://postgres.ibhcxtnwnonsnycfgjay:Hoje#estamos#fortes#como#geleia@aws-0-sa-east-1.pooler.supabase.com:5432/postgres')
+engine = create_engine('postgresql://postgres.ibhcxtnwnonsnycfgjay:Hoje#estamos#firmes#como#geleia@aws-0-sa-east-1.pooler.supabase.com:5432/postgres')
 #engine = create_engine('postgresql://postgres:Lula#2022@localhost:5432/postgres')
-sql = 'SELECT * FROM caninos'
+sql = "SELECT * FROM caninos"
 df = pd.read_sql_query(sql, con=engine)
 st.dataframe(df)
 
