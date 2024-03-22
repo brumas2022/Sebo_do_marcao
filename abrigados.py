a4234d51a1e5ae2847223530af6737a9702d9ae4
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-03-22 16:04:12 -0400

    Create abrigados.py

---
+++ abrigados.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import psycopg2 as pg
+from sqlalchemy import create_engine, text
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+st.header("Animais abrigados na ARPAA")
+
+    engine = create_engine('postgres://postgres.ibhcxtnwnonsnycfgjay:Hoje#estamos#fortes#como#geleia@aws-0-sa-east-1.pooler.supabase.com:5432/postgres')
+    #engine = create_engine('postgresql://postgres:Lula#2022@localhost:5432/postgres')
+    sql = 'SELECT * FROM caninos'
+    df = pd.read_sql_query(sql, con=engine)
+    st.dataframe(df)
+
