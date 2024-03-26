ad4088c3ee3961555d2e16617b389b9a842e3244
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-03-26 16:18:52 -0400

    Update abrigados.py

--- abrigados.py
+++ abrigados.py
@@ -19,7 +19,7 @@ col1, col2, col3, col4 = st.columns((1,1,1,1))
 col1.image(df['foto'][33], caption=df['nome'][33], use_column_width="always")
 col2.image(df['foto'][2], caption=df['nome'][2], use_column_width="always")
 col3.image(df['foto'][12], caption=df['nome'][12], use_column_width="always")
-col4.image(df['foto'][12], caption=df['nome'][12], use_column_width="always")
+col4.image(df['foto'][18], caption=df['nome'][18], use_column_width="always")
 
 col1.image(df['foto'][50], caption=df['nome'][50], use_column_width="always")
 col2.image(df['foto'][51], caption=df['nome'][51], use_column_width="always")
@@ -27,5 +27,6 @@ col3.image(df['foto'][52], caption=df['nome'][52], use_column_width="always")
 col4.image(df['foto'][53], caption=df['nome'][53], use_column_width="always")
 
 col1.image(df['foto'][54], caption=df['nome'][54], use_column_width="always")
-
-
+col1.image(df['foto'][54], caption=df['nome'][54], use_column_width="always")
+col1.image(df['foto'][54], caption=df['nome'][54], use_column_width="always")
+col1.image(df['foto'][54], caption=df['nome'][54], use_column_width="always")
