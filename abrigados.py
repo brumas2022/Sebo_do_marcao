cdd66fe5e2cb2fc75c5a94a6320c767400fe4250
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-03-27 10:01:29 -0400

    Update abrigados.py

--- abrigados.py
+++ abrigados.py
@@ -13,25 +13,28 @@ sql = "SELECT * FROM caninos"
 df = pd.read_sql_query(sql, con=engine)
 #df1 = df.sort_values
 
-st.sidebar.radio("ESCOLHA A OPCAO DESEJADA  :", ("TABELA COMPLETA", "TABELA COM FOTO", "MURAL DE FOTOS"))
-                 
-st.dataframe(df)
-colunas = ['id','nome','foto']
-resultado = df.loc[:,colunas]
-st.data_editor(resultado,column_config={"foto": st.column_config.ImageColumn("Preview Image", help="Streamlit app preview screenshots")}, hide_index=True,)
+escolha = st.sidebar.radio("ESCOLHA A OPCAO DESEJADA  :", ("TABELA COMPLETA", "TABELA COM FOTO", "MURAL DE FOTOS"))
 
-col1, col2, col3, col4 = st.columns((1,1,1,1))
-col1.image(df['foto'][33], caption=df['nome'][33], use_column_width="always")
-col2.image(df['foto'][2], caption=df['nome'][2], use_column_width="always")
-col3.image(df['foto'][12], caption=df['nome'][12], use_column_width="always")
-col4.image(df['foto'][6], caption=df['nome'][6], use_column_width="always")
-
-col1.image(df['foto'][50], caption=df['nome'][50], use_column_width="always")
-col2.image(df['foto'][51], caption=df['nome'][51], use_column_width="always")
-col3.image(df['foto'][52], caption=df['nome'][52], use_column_width="always")
-col4.image(df['foto'][53], caption=df['nome'][53], use_column_width="always")
-
-col1.image(df['foto'][54], caption=df['nome'][54], use_column_width="always")
-col2.image(df['foto'][3], caption=df['nome'][3], use_column_width="always")
-col3.image(df['foto'][5], caption=df['nome'][5], use_column_width="always")
-col4.image(df['foto'][7], caption=df['nome'][7], use_column_width="always")
+if escolha=="TABELA COMPLETA":
+   st.dataframe(df)
+if escolha=="TABELA COM FOTO":
+   colunas = ['id','nome','foto']
+   resultado = df.loc[:,colunas]
+   st.data_editor(resultado,column_config={"foto": st.column_config.ImageColumn("Preview Image", help="Streamlit app preview screenshots")}, hide_index=True,)
+if escolha=="MURAL DE FOTOS":
+  
+   col1, col2, col3, col4 = st.columns((1,1,1,1))
+   col1.image(df['foto'][33], caption=df['nome'][33], use_column_width="always")
+   col2.image(df['foto'][2], caption=df['nome'][2], use_column_width="always")
+   col3.image(df['foto'][12], caption=df['nome'][12], use_column_width="always")
+   col4.image(df['foto'][6], caption=df['nome'][6], use_column_width="always")
+    
+   col1.image(df['foto'][50], caption=df['nome'][50], use_column_width="always")
+   col2.image(df['foto'][51], caption=df['nome'][51], use_column_width="always")
+   col3.image(df['foto'][52], caption=df['nome'][52], use_column_width="always")
+   col4.image(df['foto'][53], caption=df['nome'][53], use_column_width="always")
+    
+   col1.image(df['foto'][54], caption=df['nome'][54], use_column_width="always")
+   col2.image(df['foto'][3], caption=df['nome'][3], use_column_width="always")
+   col3.image(df['foto'][5], caption=df['nome'][5], use_column_width="always")
+   col4.image(df['foto'][7], caption=df['nome'][7], use_column_width="always")
