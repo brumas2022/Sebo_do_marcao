f136b7867c6cdc295c3109df9c4dac9921e53460
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-04-03 09:11:33 -0400

    Atualizar o abrigados.py

--- abrigados.py
+++ abrigados.py
@@ -5,13 +5,13 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 
-st.header("Animais abrigados na ARPAA")
+st.header("Animais na ARPAA")
 
 engine = create_engine('postgresql://postgres.ibhcxtnwnonsnycfgjay:Hoje#estamos#firmes#como#geleia@aws-0-sa-east-1.pooler.supabase.com:5432/postgres')
 #engine = create_engine('postgresql://postgres:Lula#2022@localhost:5432/postgres')
 sql = "SELECT * FROM caninos"
 df = pd.read_sql_query(sql, con=engine)
-
+lista = ["TABELA COMPLETA","TABELA COM FOTO", "MURAL DE FOTOS"]
 
 escolha = st.sidebar.radio("ESCOLHA A OPCAO DESEJADA  :", ("TABELA COMPLETA", "TABELA COM FOTO", "MURAL DE FOTOS"))
 
