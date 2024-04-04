5b56de35b3b30244a9cc70192e827245885f96a1
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-04-04 14:33:55 -0400

    Update abrigados.py

--- abrigados.py
+++ abrigados.py
@@ -24,6 +24,14 @@ if escolha=="TABELA COM FOTO":
    st.data_editor(resultado,column_config={"foto": st.column_config.ImageColumn("Preview Image", help="Streamlit app preview screenshots")}, hide_index=True,)
    df_filtro = df[df["foto"]!=""]
    st.dataframe(df_filtro.set_index('id'))
+
+   col1, col2, col3, col4 = st.columns((1,1,1,1))
+   col1.image(df_filtro['foto'][54], caption=df_filtro['nome'][54], use_column_width="always")
+   col2.image(df_filtro['foto'][32], caption=df_filtro['nome'][32], use_column_width="always")
+   col3.image(df_filtro['foto'][51], caption=df_filtro['nome'][51], use_column_width="always")
+   col4.image(df_filtro['foto'][41], caption=df_filtro['nome'][41], use_column_width="always")
+
+
 if escolha=="MURAL DE FOTOS":
 
    from supabase import create_client, Client
