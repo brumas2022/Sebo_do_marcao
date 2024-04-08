acde83672c872901100f64948124a9d9d71a42f8
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-04-08 10:56:10 -0400

    Update index.py

--- index.py
+++ index.py
@@ -53,10 +53,11 @@ cols = st.columns((1,1))
 cols[0].link_button("Pague menos", "https://mais.app/1Nm5y9", use_container_width=True)
 cols[0].link_button("Memorando SANEAR", "https://comunicasanear-avcnrpqesbpzeg2ecsmuun.streamlit.app/", use_container_width=True)
 cols[0].link_button("Cadastro de animais", "https://causanimal.streamlit.app", use_container_width=True)
+cols[0].link_button("Galeria de fotos", "https://galeria.streamlit.app", use_container_width=True)
 cols[1].link_button("Video do Youtube", "https://www.youtube.com/watch?v=JnxCIRxt3kQ&t=19s", use_container_width=True)
 cols[1].link_button("Faça sua doação", "https://www.mercadopago.com.br/integrations/v1/web-payment-checkout.js", use_container_width=True)
 cols[1].link_button("Lojinha", "https://loja1dosebo.streamlit.app/", use_container_width=True)
-
+cols[1].link_button("Aciona Telegram", "https://album.streamlit.app/", use_container_width=True)
 
 
 a = st.selectbox("Escolha a opção desejada",("Mensagem para o Marcao", "Desejar bom dia ao Marcao", "Desejar boa noite ao Marcao"))
