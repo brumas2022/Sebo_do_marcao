31bf0c11ac27a17957222be0448ec454751a22fb
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-05-13 09:13:51 -0400

    Create anotacoes.py

---
+++ anotacoes.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+st.header("Anotações do curso de Historia e Especialização em Historia Pública")
+st.write("O que eram os escravos de ganho e escravos de aluguel")
