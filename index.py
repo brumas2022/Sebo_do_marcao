588c4eff0e8a8260f0860e714df9529733eb0659
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-05-20 16:40:59 -0400

    Update index.py

--- index.py
+++ index.py
@@ -63,6 +63,7 @@ cols[1].link_button("Video do Youtube", "https://www.youtube.com/watch?v=JnxCIRx
 cols[1].link_button("Faça sua doação", "https://www.mercadopago.com.br/integrations/v1/web-payment-checkout.js", use_container_width=True)
 cols[1].link_button("Lojinha", "https://loja1dosebo.streamlit.app/", use_container_width=True)
 cols[1].link_button("Aciona Telegram", "https://sebodomarcao-g4cnpbtuc8aclncvnvzr4e.streamlit.app/", use_container_width=True)
+cols[1].link_button("Sobre museu Rosa Bororo - fonte: ATribunamt", "https://www.atribunamt.com.br/opiniao-do-leitor/2024/05/entre-o-passado-e-o-futuro-o-papel-do-museu-rosa-bororo-na-historia-em-rondonopolis/", use_container_width=True)
 
 
     
