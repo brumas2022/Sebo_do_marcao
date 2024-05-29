29df3f4c3d4db26872235e40a05f7705dd86ac4d
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-05-29 10:41:09 -0400

    Update index.py

--- index.py
+++ index.py
@@ -40,42 +40,14 @@ st.write("---")
 #st.image("IMG_20220616_220024.jpg")
 st.image("crie_uma_imagem_de_um_violao_inteirona_diagonal_ladeado_por_livros_e_notas_musicais_png.png")
 
-#cols1 = st.columns((1,1,1))
-#cols1[0].image("livros-stack-realistic_1284-4735.jpg")
-#cols1[1].image("livros-stack-realistic_1284-4735.jpg")
-#cols1[2].image("livros-stack-realistic_1284-4735.jpg")
-
-#cols2 = st.columns((1,1,1,1,1,1,1))
-#cols2[0].image("simbolos-musicais-ondulam-em-fundo-branco_1308-77408.jpg")
-#cols2[1].image("simbolos-musicais-ondulam-em-fundo-branco_1308-77408.jpg")
-#cols2[2].image("simbolos-musicais-ondulam-em-fundo-branco_1308-77408.jpg")
-#cols2[3].image("simbolos-musicais-ondulam-em-fundo-branco_1308-77408.jpg")
-#cols2[4].image("simbolos-musicais-ondulam-em-fundo-branco_1308-77408.jpg")
-#cols2[5].image("simbolos-musicais-ondulam-em-fundo-branco_1308-77408.jpg")
-#cols2[6].image("simbolos-musicais-ondulam-em-fundo-branco_1308-77408.jpg")
 
 
-                   
-                   
-
-
-
-
-cols = st.columns((1,1))
-cols[0].link_button("Pague menos", "https://mais.app/1Nm5y9", use_container_width=True)
-cols[0].link_button("Memorando SANEAR", "https://comunicasanear-avcnrpqesbpzeg2ecsmuun.streamlit.app/", use_container_width=True)
-cols[0].link_button("Cadastro de animais", "https://causanimal.streamlit.app", use_container_width=True)
-cols[0].link_button("Galeria de fotos", "https://galeria.streamlit.app", use_container_width=True)
-cols[0].link_button("Anotações Historia", "anotacoes.py", use_container_width=True)
-cols[1].link_button("Video do Youtube", "https://www.youtube.com/watch?v=JnxCIRxt3kQ&t=19s", use_container_width=True)
-cols[1].link_button("Faça sua doação", "https://www.mercadopago.com.br/integrations/v1/web-payment-checkout.js", use_container_width=True)
-cols[1].link_button("Lojinha", "https://loja1dosebo.streamlit.app/", use_container_width=True)
-cols[1].link_button("Aciona Telegram", "https://sebodomarcao-g4cnpbtuc8aclncvnvzr4e.streamlit.app/", use_container_width=True)
-cols[1].link_button("Sobre museu Rosa Bororo - fonte: ATribunamt", "https://www.atribunamt.com.br/opiniao-do-leitor/2024/05/entre-o-passado-e-o-futuro-o-papel-do-museu-rosa-bororo-na-historia-em-rondonopolis/", use_container_width=True)
-
-
-    
-
+cols = st.columns((1,1,1,1,1))
+cols[0].link_button("F1", "https://www.uol.com.br", use_container_width=True)
+cols[1].link_button("F2", "https://www.sanearmt.com.br", use_container_width=True)
+cols[2].link_button("F3", "https://www.rondonopolis.mt.gov.br", use_container_width=True)
+cols[3].link_button("F4", "https://www.atribunamt.com.br", use_container_width=True)
+cols[4].link_button("F5", "https://www.agoramt.com.br", use_container_width=True)
 
 
 a = st.selectbox("Escolha a opção desejada",("Mensagem para o Marcao", "Desejar bom dia ao Marcao", "Desejar boa noite ao Marcao"))
