01c2e1b95fddecbb51036f485f725cab974013da
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-06-05 15:32:39 -0400

    Atualizar o index.py

--- index.py
+++ index.py
@@ -15,7 +15,7 @@ if senha=="Programas":
     st.sidebar.link_button("Memorando SANEAR", "https://comunicasanear-avcnrpqesbpzeg2ecsmuun.streamlit.app/", use_container_width=True)
     st.sidebar.link_button("Cadastro de animais", "https://causanimal.streamlit.app", use_container_width=True)
     st.sidebar.link_button("Galeria de fotos", "https://galeria.streamlit.app", use_container_width=True)
-    st.sidebar.link_button("Anotações Historia", "anotacoes.py", use_container_width=True)
+    st.sidebar.link_button("Plano de Saneamento", "https://drive.google.com/file/d/1yDofmyFtOFxvmvhrWFx-ql-Odxt5Mdho/view", use_container_width=True)
     st.sidebar.link_button("Video do Youtube", "https://www.youtube.com/watch?v=JnxCIRxt3kQ&t=19s", use_container_width=True)
     st.sidebar.link_button("Faça sua doação", "https://www.mercadopago.com.br/integrations/v1/web-payment-checkout.js", use_container_width=True)
     st.sidebar.link_button("Lojinha", "https://loja1dosebo.streamlit.app/", use_container_width=True)
