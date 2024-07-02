2f247c8086d42f741f469ffe56f45968e359189c
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-07-01 21:14:02 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from st_pages import show_pages_from_config, add_page_title
 
-#add_page_title()
+#add_page_title()  
 #show_pages_from_config(".streamlit/pages.toml")
 
 senha = st.sidebar.selectbox(
