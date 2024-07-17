8342e4dcf8cdc356c4d5a994f0960ab20a2f2da3
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-07-16 23:14:15 -0300

    Create planeja.py

---
+++ planeja.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+import supabase as supa
+lista = ["Acoes", "Responsavel", "Atrasados", "Em dia"]
+escolha = st.selectbox("Escolha uma opcao : ", lista)
+if escolha == "Acoes":
+  st.write("Estas são as XX ações do planejamento estratégico 2024")
+  st.selectbox("Es
+if escolha == "Responsavel":
+   list_resp = ["João Couto", "Denize"]
+   resp = st.radio("Escolha o responsavel para listar todaas as suas ações", list_resp)
+   if resp == "João Couto":
+      st.write("Este é o João Couto")
+      #st.image()
+   if resp == "Denize":
+      st.write("Esta é a Denize")
+      #st.image()
+   #fazer o filtro no supabse com o resp
+if escolha == "Atrasados":
+   st.write("Estas ações estão com os seguinte atrasos")
+if escolha == "Em dia"
+   st.write("Estas ações estão em dia")
+
+
+  
