2375c7b416d901e677309153d1498414355077ba
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-07-18 08:47:05 -0400

    Update planeja.py

--- planeja.py
+++ planeja.py
@@ -6,6 +6,10 @@ st.sidebar.title("Grupos")
 
 def cliq_5():
       st.dataframe(df5.iloc[4:])
+def cliq_6():
+      st.dataframe(df6.iloc[4:])
+def cliq_7():
+      st.dataframe(df7.iloc[4:])
 
 
 g1=st.sidebar.button("GRUPO 01")
@@ -61,8 +65,8 @@ if g1:
   col1[2].image("WILMA_MUNDIM.png", width=200, use_column_width=True)
   col1[3].image("EDUARDO_RAMOS.png", width=200, use_column_width=True)
   col1[0].button(acao5+" - "+nome_acao5, on_click=cliq_5 ,use_container_width=True)
-  col1[0].button(acao6+" - "+nome_acao6)
-  col1[0].button(acao7+" - "+nome_acao7)
+  col1[0].button(acao6+" - "+nome_acao6, on_click=cliq_6, use_container_width=True)
+  col1[0].button(acao7+" - "+nome_acao7, on_click=cliq_7, use_container_width=True)
   col1[1].button(acao4+" - "+nome_acao4)
   col1[1].button(acao3+" - "+nome_acao3)
   col1[2].button(acao2+" - "+nome_acao2)
