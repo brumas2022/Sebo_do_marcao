c2db759912673c5efee346e7a831ed4f98002579
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-07-17 23:45:55 -0300

    Update planeja.py

--- planeja.py
+++ planeja.py
@@ -58,10 +58,14 @@ if g1:
   col1[1].button(acao5+" - "+nome_acao5)
   col1[1].button(acao6+" - "+nome_acao6)
   col1[1].button(acao7+" - "+nome_acao7)
-  col1[1].button(" ")
+  col1[1].write(" ")
   col1[1].button(acao4+" - "+nome_acao4)
   col1[1].button(acao3+" - "+nome_acao3)
+  col1[1].write(" ")
+  col1[1].write(" ")
   col1[1].button(acao2+" - "+nome_acao2)
+  col1[1].write(" ")
+  col1[1].write(" ")
   col1[1].button(acao1+" - "+nome_acao1)
   
   
