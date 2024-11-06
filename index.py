431ea050347574b0297dd46a53680d978e26b655
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-11-06 10:22:21 -0400

    Update index.py

--- index.py
+++ index.py
@@ -74,6 +74,7 @@ if senha=="Cifras":
 if senha=="Simon":
     st.sidebar.markdown(":violet[Aquele que serve a uma revolução ara no mar]")
     st.sidebar.markdown(":musical_note:")
+    st.sidebar.audio("06-Hc3", format="audip/mpeg")
 
 
 st.header(":green[Sebo do Marcão]", divider="orange")
